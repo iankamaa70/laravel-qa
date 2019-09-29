c945cebcd07b02be534beb536abcf988a1e36dd6
root <iankamaa70@gmail.com>
2019-09-29 15:43:58 +0300

    new commit

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
